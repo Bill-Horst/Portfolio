cbd63a903cb22b9a79843187fffd695d500f597a
Bill Horst <bill@billhorst.com>
2018-03-07 22:59:35 -0700

    added English teacher app to projects

--- js/work.js
+++ js/work.js
@@ -2,7 +2,7 @@ let works = [
   // FRONT PAGE PICS SHOULD BE WIDTH:952 BY HEIGHT:600
   // FEATURE PICS SHOULD BE WIDTH:238 BY HEIGHT:150 EACH
   {
-    id: 'in-progress',
+    id: 'chamisama-tea',
     title: 'Chamisama - Japanese Tea Ecommerce App',
     hoverDescription: 'This is a Rails ecommerce app for selling Japanese and Chinese tea.',
     techUsed: 'HTML / CSS / JavaScript / jQuery / Ruby / Rails 5 / Stripe',
@@ -10,6 +10,16 @@ let works = [
     featureImage: 'img/project_photos/chamisama_features.jpg',
     githubLink: 'https://github.com/Bill-Horst/chamisama',
     href: 'https://chamisama.herokuapp.com/'
+  },
+  {
+    id: 'hello-sensei',
+    title: 'Hello Sensei - ESL / EFL Teacher Applicaion',
+    hoverDescription: 'This is an AngularJS front / Rails 5 back API application for teachers.',
+    techUsed: 'HTML / CSS / JavaScript / AngularJS / TinyMCE / Ruby / Rails 5',
+    frontPageImage: 'img/project_photos/hello_front.jpg',
+    featureImage: 'img/project_photos/hello_features.jpg',
+    githubLink: 'https://github.com/Bill-Horst/ELT-Angular-Front',
+    href: 'http://elt-app.s3-website-us-west-2.amazonaws.com/#/'
   },
   {
     id: 'plot-generator',
